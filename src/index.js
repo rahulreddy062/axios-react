1e01be93ffc760c2c1fe76d07efa228a06b5cdc8
rahul reddy <42753261+rahulreddy062@users.noreply.github.com>
2020-01-03 15:32:05 -0600

    Add axios to react

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+
+axios.interceptors.request.use(request=>
+    {
+        console.log(request);
+        // Edit the request config Ex: Add headers 
+        // Always return the request config
+        return request;
+    },error=>{
+        console.log(error);
+        return Promise.reject(error);
+    });
+
+axios.interceptors.response.use(response=>{
+    console.log(response);
+    return response;
+},error=>{
+    console.log(error);
+    return Promise.reject(error);
+})
+ReactDOM.render( <App />, document.getElementById( 'root' ) );
+registerServiceWorker();
