362f5d0cb1744cfacc34c7db68156c3679e71afa
rahul reddy <42753261+rahulreddy062@users.noreply.github.com>
2020-01-05 16:36:40 -0600

    Add nav items with axios

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import "./Blog.css";
 import Posts from './Posts/Posts';
-import {Route,Link} from 'react-router-dom';
+import {Route, NavLink,Switch} from 'react-router-dom';
 import NewPosts from './NewPost/NewPost';
+import FullPost from './FullPost/FullPost';
 class Blog extends Component {
   state = {
     posts: [],
@@ -19,18 +20,23 @@ class Blog extends Component {
       <header>
         <nav>
           <ul>
-            <li> <Link to="/">Home</Link></li>
-            <li> <Link to = {{
+            <li> <NavLink 
+                  to="/"
+                  exact >Posts</NavLink></li>
+            <li> <NavLink to = {{
               pathname:'/new-post',
               hash:'#submit',
               search:'?quick-submit=true'
-            }}>New Post</Link></li>
+            }}>New Post</NavLink></li>
           </ul>
         </nav>
       </header>
       {/* <Route path="/" exact render={()=><h1>Home</h1>}/> */}
-      <Route path = '/' exact component = {Posts}/>
-      <Route path = '/new-post' component = {NewPosts}/>
+      <Switch>
+       <Route path = '/'  exact component = {Posts}/>
+      <Route path = '/new-post' exact component = {NewPosts}/>
+      <Route path = '/:id' exact  component = {FullPost}/>
+      </Switch>
       </div>
     );
   }
--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,48 +1,51 @@
-import React,{Component} from 'react';
-import axios from '../../../axios';
-import Post from '../../../components/Post/Post';
+import React, { Component } from "react";
+import axios from "../../../axios";
+import Post from "../../../components/Post/Post";
 class Posts extends Component {
-    state = {
-        posts: []
-      };
-      componentDidMount() {
-          console.log(this.props);
-        axios.get("/posts").then(response => {
-            const posts = response.data.slice(0,4);
-            const updatedPosts = posts.map(post=>{
-                return {
-                    ...post,
-                    author:'Rahul'
-                }
-            })
-          this.setState({ posts: updatedPosts });
-          
-        })
-        .catch(error=>{
-          this.setState({error:true});
+  state = {
+    posts: []
+  };
+  componentDidMount() {
+    console.log(this.props);
+    axios
+      .get("/posts")
+      .then(response => {
+        const posts = response.data.slice(0, 4);
+        const updatedPosts = posts.map(post => {
+          return {
+            ...post,
+            author: "Rahul"
+          };
         });
-      }
-      postSelectedHandler = (id) =>{
-        this.setState({selectedPostId:id})
-    }
-    render(){
-        let posts = <p style ={{textAlign:'center'}}>Something went wrong</p>
-    if(!this.state.error){
+        this.setState({ posts: updatedPosts });
+      })
+      .catch(error => {
+        this.setState({ error: true });
+      });
+  }
+  postSelectedHandler = id => {
+    this.props.history.push({
+        pathname:'/' + id
+    });
+
+  };
+  render() {
+    let posts = <p style={{ textAlign: "center" }}>Something went wrong</p>;
+    if (!this.state.error) {
       posts = this.state.posts.map(post => {
-        return <Post
-         author = {post.author}
-          key = {post.title}
-          title={post.title}
-          
-          clicked = {()=>this.postSelectedHandler(post.id)} />;
+        return (
+        //   <Link to={"/posts/" + post.id}>
+            <Post
+             key={post.id}
+              author={post.author}
+              title={post.title}
+              clicked={() => this.postSelectedHandler(post.id)}
+            />
+        //   </Link>
+        );
       });
     }
-        return (
-            <section className="Posts">
-        {posts}
-        </section>
-        );
-
-    }
+    return <section className="Posts">{posts}</section>;
+  }
 }
 export default Posts;
